778d79c5c0f02460f3caa4f84e2b543695957722
rayzhang-xingyunzh <Ray@Rays-MBP.lan>
2016-07-29 16:48:15 +0800

    re-org

---
+++ app/routes/user-api.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+
+var userController = require('../controllers/userController');
+
+router.post('/', userController.logStatus);
+
+module.exports = router;
--- routes/users.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-
-var user_controller = require('../controllers/userController');
-
-router.post('/', user_controller.logStatus);
-
-module.exports = router;
