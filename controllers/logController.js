87f9ee1daeed8b12d638342cfc15c03a54c599ae
morrieati <morrislew@outlook.com>
2016-07-29 17:04:58 +0800

    user verification ok

--- controllers/logController.js
+++ controllers/logController.js
@@ -1,7 +1,19 @@
 /**
  * Created by morrieati on 7/28/16.
  */
+var request = require('request');
+
 exports.verifyID = function (req, res, next) {
-    console.log(req.body.username);
+    request.post('http://www.xingyunzh.com:3001/users/verify', {
+        form: {
+            'username': req.body.username,
+            'password': req.body.password
+        }
+    }, function (error, response, body) {
+        console.log(body);
+    });
+
+
+    //console.log(req.body.username);
     res.send("ok");
 };
