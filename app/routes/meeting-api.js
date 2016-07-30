cc4428bb793617a48a74b40c5e0e2e083e57ebf0
Hahn.chen <hahn.chen@xingyunzh.com>
2016-07-30 14:15:14 +0800

    up

--- app/routes/meeting-api.js
+++ app/routes/meeting-api.js
@@ -3,6 +3,10 @@ var router = express.Router();
 
 var meetingController = require('../controllers/meetingController');
 
+router.get('/', function (req, res, next) {
+    res.render('setting', {title: 'Setting'});
+});
+
 router.post('/create', meetingController.createMeeting);
 
 module.exports = router;
