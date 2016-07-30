560ba6df402455c3c1226a4005a2f335bde07623
Morris Lew <morrislew@outlook.com>
2016-07-31 00:11:12 +0800

    feature: Add user's information to local database

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -11,18 +11,39 @@ exports.verifyID = function (req, res, next) {
             'password': password
         }
     }, function (error, response, body) {
-        console.log(body);
+        if (body != undefined) {
+            var userBody = JSON.parse(body).body;
+            User
+                .find({username: userBody.username})
+                .exec(function (err, docs) {
+                    if (docs.length != 0) {
+                        User.update(
+                            {username: userBody.username},
+                            {lastLoginDate: new Date()},
+                            {multi: true},
+                            function (err, rawResponse) {
+                                console.log(rawResponse);
+                            }
+                        );
+                        console.log('User Logged In!');
+                    } else {
+                        var user = new User({
+                            username: userBody.username,
+                            name: userBody.name,
+                            sex: userBody.sex,
+                            createdDate: new Date(),
+                            lastLoginDate: new Date()
+                        });
+                        user.save(function (err, user) {
+                            if (err) return console.error(err);
+                        });
+                        console.log("New User Created!");
+                    }
+                });
+        } else {
+            console.log("Verification failed!");
+        }
     });
-
-    var user = new User({
-        name: username,
-        password: password,
-        date: new Date()
-    });
-    user.save(function (err, user) {
-        if (err) return console.error(err);
-    });
-    console.log("Log Saved");
 
     res.send("ok");
 };
