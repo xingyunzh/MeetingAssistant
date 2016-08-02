0bc62a92b62835de1a3ddf827bcc1f78fcf65344
halipeter <peter.cheng@xingyunzh.com>
2016-08-02 13:34:31 +0800

    Changes

--- app/util/sendmailexm.js
+++ app/util/sendmailexm.js
@@ -1,7 +1,7 @@
 /**
  * Created by asus on 2016/7/30 0030.
  */
-var sendmailController = require("../controllers/sendmailController")
+var sendmailController = require("./../controllers/sendmailController")
 var options= new Object ();
 options.torecodermail='peter.cheng@xingyunzh.com';
 options.toconventionermail='peter.cheng@xingyunzh.com';
@@ -10,6 +10,6 @@ options.place='summerbooks';
 options.meetingname='Meeting name';
 options.des='Meeting descriptions';
 options.agen='xxxxxxxxxxxxxxxxxxxxxxxxx' +
-    'xxxxxxxxxxxxxxxxxxxxx'
+    'xxxxxxxxxxxxxxxxxxxxx';
 
 sendmailController.sendmail(options);
