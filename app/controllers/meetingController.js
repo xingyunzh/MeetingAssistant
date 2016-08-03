adbfeede983046911dec1f835d8aefd208170c4e
rayzhang-xingyunzh <ray.zhang@xingyunzh.com>
2016-08-03 18:46:38 +0800

    getMeetingById

--- app/controllers/meetingController.js
+++ app/controllers/meetingController.js
@@ -1,12 +1,16 @@
 var Meeting = require("../models/meeting");
+var errorHandler = require('../util/shared/errorHandler').errorHandler;
 var util = require("../util/shared/util");
 
-exports.createMeeting = function (req, res) {
+exports.createMeetingById = function (req, res) {
 
 };
 
+exports.updateMeetingById = function (req, res) {
 
-exports.updateMeeting = function (req, res) {
+};
+
+exports.deleteMeetingById = function (req, res) {
 
 };
 
@@ -15,7 +19,20 @@ exports.getMeetingsByUserId = function(req,res) {
 };
 
 exports.getMeetingById = function(req,res) {
-	
+	var meetingId = req.body.meetingId;
+
+	Meeting
+		.findOne(meetingId)
+		.populate('host')
+		.populate('UMJunction')
+		.populate('recorder')
+		.exec(function(err,result){
+			if (err) {
+				errorHandler(err);
+			}else{
+				res.send(util.wrapBody(result,'S'));
+			}
+		});
 };
 
 exports.getDoneMeetingNum = function (req, res) {
