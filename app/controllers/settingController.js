e4d30bb06d86486f20a3829f81a46e7da39984b2
Hahn.chen <hahn.chen@xingyunzh.com>
2016-08-04 16:02:59 +0800

    hostview

---
+++ app/controllers/settingController.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Administrator on 2016/8/2.
+ */
+exports.createsetting = function (req, res) {
+
+}
--- app/routes/api-router.js
+++ app/routes/api-router.js
@@ -5,10 +5,14 @@ var loginAPI = require("./login-api");
 var userAPI = require("./user-api");
 var meetingAPI = require("./meeting-api");
 var senmailAPI=require("./sendmail-api");
+var settingAPI=require("./setting-api");
+var hostviewAPI=require("./hostview-api");
 
 router.use("/", loginAPI);
 router.use("/meeting", meetingAPI);
 router.use("/user", userAPI);
 router.use("/sendmail",senmailAPI);
+router.use("/setting",settingAPI);
+router.use("/hostview",hostviewAPI);
 
 module.exports = router;
