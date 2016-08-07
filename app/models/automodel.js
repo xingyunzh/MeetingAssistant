f8794806672df7469f2455a5e7febb50b23131c8
Hahn.chen <hahn.chen@xingyunzh.com>
2016-08-07 13:40:28 +0800

    delete

--- app/models/automodel.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-var automodelSchema = mongoose.Schema({
-    email: String,
-
-
-});
-
-module.exports = mongoose.model('automodel', automodelSchema);
--- app/routes/auto-api.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-
-var automodelController = require("../controllers/automodelController");
-
-router.post('/autofun', sendmailController.autofun);
-
-module.exports = router;
