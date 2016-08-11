45f16eec3a6c513a7abd439556da167e6bcce194
Anklejiang <ankle.jiang@xingyunzh.com>
2016-08-11 16:11:42 +0800

    I have complished my task.

--- app/routes/api-router.js
+++ app/routes/api-router.js
@@ -5,23 +5,11 @@ var router = express.Router();
 var loginAPI = require("./login-api");
 var userAPI = require("./user-api");
 var meetingAPI = require("./meeting-api");
-
-
 var sendmailAPI = require("./sendmail-api");
 var viewAPI = require("./view-api");
-
-var autoAPI = require('./auto-api');
-var settingAPI=require("./setting-api");
-var hostviewAPI=require("./hostview-api");
-
-
-var sendmailAPI = require("./sendmail-api");
-var viewAPI = require("./view-api");
-
-
-router.use("/verification", loginAPI);
 
 router.use("/", viewAPI);
+router.use("/verification", loginAPI);
 
 router.use(token.Parser);
 
